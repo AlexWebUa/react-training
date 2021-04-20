faf4144f7daa55530c53fa0857a5ed83652f93d5
Aleksandr <alexwebua1@gmail.com>
2021-04-21 00:24:30 +0300

    Added dialogs page

--- src/components/Main/Dialogs/Dialogs.jsx
+++ src/components/Main/Dialogs/Dialogs.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import "./Dialogs.scss";
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className="dialogs">
-            Dialogs
+            <div className="dialogs__list">
+                <div className="dialog">
+                    <NavLink to="/dialogs/1">Name1</NavLink>
+                </div>
+                <div className="dialog">
+                    <NavLink to="/dialogs/2">Name2</NavLink>
+                </div>
+                <div className="dialog active">
+                    <NavLink to="/dialogs/3">Name3</NavLink>
+                </div>
+                <div className="dialog">
+                    <NavLink to="/dialogs/4">Name4</NavLink>
+                </div>
+                <div className="dialog">
+                    <NavLink to="/dialogs/5">Name5</NavLink>
+                </div>
+            </div>
+            <div className="dialogs__messages">
+                <div className="message">
+                    Test
+                </div>
+                <div className="message">
+                    Hello world!
+                </div>
+                <div className="message">Lorem ipsum</div>
+            </div>
         </div>
     );
 }
